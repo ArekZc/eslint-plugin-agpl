86e32d28cd5ed7b95495cc5bcec941d63fe00771
a.zajac <a.zajac@clearcode.cc>
2015-06-17 11:21:50 +0200

    fix wrong license string

--- tests/lib/rules/valid-license.js
+++ tests/lib/rules/valid-license.js
@@ -29,7 +29,7 @@ eslintTester.addRuleTest('lib/rules/valid-license', {
         ' * This program is free software: you can redistribute it and/or modify it under\n' +
         ' * the terms of the GNU Affero General Public License as published by the Free\n' +
         ' * Software Foundation, either version 3 of the License, or (at your option) any\n' +
-        ' * later version\n' +
+        ' * later version.\n' +
         ' *\n' +
         ' * This program is distrubuted in the hope that it will be useful,\n' +
         ' * but WITHOUT ANY WARRANTY; without even the implied warranty of\n' +
